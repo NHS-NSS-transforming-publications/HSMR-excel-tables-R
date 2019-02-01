32ddb8213d4d8839b2904e239f8a9eaa7ed91f44
TinaFu <31853064+Tina815@users.noreply.github.com>
2019-02-01 15:06:29 +0000

    Add files via upload

--- HSMR_Excel.R
+++ HSMR_Excel.R
@@ -3,31 +3,59 @@ library(readr)
 library(openxlsx)
 library(dplyr)
 
-#For TableA1a
+
+# For TableA1a----
 hsmr_data <- read_csv("smr_data.csv") 
-hsmr_data <- hsmr_data %>% filter(location_type == "Scotland"|location_type == "NHS Board")
+hsmr_data_A1a <- hsmr_data %>% filter(location_type == "Scotland"|location_type == "NHS Board")
 
 ## load existing workbook
 wb <- loadWorkbook("TableA1a_Tina.xlsm")
 
 # write to the workbook
-writeData(wb, "smr_data", hsmr_data, startCol = 2, startRow = 1, colNames = TRUE)
+writeData(wb, "smr_data", hsmr_data_A1a, startCol = 2, startRow = 1, colNames = TRUE)
 
 # Save workbook     
 saveWorkbook(wb, "TableA1a_Tina.xlsm", overwrite = TRUE)
 
-#For TableA2a
-long_trend_data <- read_csv("long_term_trends (1).csv") 
-long_trend_data <- long_trend_data %>% filter(HB2014 == "Scotland")
+
+# For Table2----
+hsmr_data_2 <- hsmr_data %>% filter(location_type == "Scotland"|location_type == "hospital")
 
 ## load existing workbook
-wb <- loadWorkbook("TableA2a_Tina_test.xlsm")
+wb <- loadWorkbook("Table2.xlsx")
 
 # write to the workbook
-writeData(wb, "long_trend_data", long_trend_data, startCol = 2, startRow = 1, colNames = TRUE)
+writeData(wb, "smr_data", hsmr_data_2, startCol = 2, startRow = 1, colNames = TRUE)
 
 # Save workbook     
-saveWorkbook(wb, "TableA2a_Tina_test.xlsm", overwrite = TRUE)
+saveWorkbook(wb, "Table2.xlsx", overwrite = TRUE)
+
+
+# For Table3----
+hsmr_data_3 <- hsmr_data %>% filter(location_type == "Scotland"|location_type == "hospital")
+
+## load existing workbook
+wb <- loadWorkbook("Table3.xlsx")
+
+# write to the workbook
+writeData(wb, "smr_data", hsmr_data_3, startCol = 2, startRow = 1, colNames = TRUE)
+
+# Save workbook     
+saveWorkbook(wb, "Table3.xlsx", overwrite = TRUE)
+
+
+# For TableA2a----
+long_trend_data <- read_csv("long_term_trends (1).csv") 
+long_trend_data_A2a <- long_trend_data %>% filter(HB2014 == "Scotland")
+
+## load existing workbook
+wb <- loadWorkbook("TableA2a_Tina.xlsx")
+
+# write to the workbook
+writeData(wb, "long_trend_data", long_trend_data_A2a, startCol = 2, startRow = 1, colNames = TRUE)
+
+# Save workbook     
+saveWorkbook(wb, "TableA2a_Tina.xlsx", overwrite = TRUE)
 
 
           
